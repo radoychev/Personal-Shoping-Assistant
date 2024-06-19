7d63c869f69def501a5580ff800a6975d3ecb0ad
liafernando <ameli.masewge.fernando@student.nhlstenden.com>
2024-06-19 13:52:03 +0200

    Added few changes for the splash Screen

--- SmartAisle/SplashScreen.swift
+++ SmartAisle/SplashScreen.swift
@@ -1,7 +1,7 @@
 import SwiftUI
-
 struct SplashScreen: View {
     @State private var isActive = false
+    @State private var textOpacity = 0.0
 
     var body: some View {
         VStack {
@@ -13,6 +13,16 @@ struct SplashScreen: View {
                         .font(.largeTitle)
                         .fontWeight(.bold)
                         .foregroundColor(.black)
+                    
+                    Text("Your personal shopping assistant")
+                        .font(.title2)
+                        .foregroundColor(.black)
+                        .opacity(textOpacity)
+                        .onAppear {
+                            withAnimation(.easeIn(duration: 1.5)) {
+                                self.textOpacity = 1.0
+                            }
+                        }
                 }
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
                 .background(Color(red: 173/255, green: 216/255, blue: 230/255))
