33afb35951e992d1d300e779ecb8ce27c0446594
Nathan P. Pais D'costa <71150111+nathan-pete@users.noreply.github.com>
2024-06-20 12:04:31 +0200

    Created Index
    Created the page using Swift with iOS dependency, SwiftUI libraries. Primarily used to test the placement of visual assets of the application, this page contains the gradient function, creating a linear gradient and applying it as a background (using stacks, where ZStack places assets on the background, HStack places items horizontally adjacent, VStack vertically stacks items, similar to that of a list.

---
+++ Index.swift
@@ -0,0 +1,27 @@
+//
+//  Index.swift
+//  SmartAisle
+//
+//  Created by Nathan Pete on 12/06/2024.
+//
+
+import SwiftUI
+import Swift
+import SwiftData
+
+let backgroundGradient = LinearGradient(
+    colors: [.bg, .fg],
+    startPoint: .bottomTrailing, endPoint: .top)
+
+
+//struct Index: View{
+//    var body: some View {
+//        
+//                Footer()
+//            }
+//        }
+//        .ignoresSafeArea()
+//    }
+//}
+
+    
